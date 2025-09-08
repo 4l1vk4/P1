90ba4f851c9cdf856d36cfcbac96362ddc56bdd8
4l1vk4 <alivkatop@mail.ru>
2025-09-08 09:08:43 +0300

    Первая версия

--- зщ1.py
+++ зщ1.py
@@ -1,31 +1,24 @@
 import shlex
 
-commands = ["ls", "cd", "exit"] #коммандыы
-
+commands = ["ls", "cd", "exit"]
 while True:
     command_line = input("VFS> ")
     try:
-        tokens = shlex.split(command_line)  #считка
+        user_input = shlex.split(command_line)
     except ValueError as err:
         print(f"Ошибка парсинга: {err}")
         continue
-    if not tokens:
+    if not user_input:
         continue
-
-    #проверка ввода
-    cmd = tokens[0]
-    args = tokens[1:]
-
+    cmd = user_input[0]
+    args = user_input[1:]
     if cmd not in commands:
         print(f"Неизвестная команда: {cmd}")
         continue
-
     if cmd == "ls":
         print()
     elif cmd == "cd":
         print()
-
-    #вывод
     elif cmd == "exit":
         print("Выход из эмулятора.")
         break
